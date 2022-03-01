653b7505c9e590c31ae2650957a47814e78d4a97
Binh. Nguyen Xuan - CMC Global DU3 <nxbinh@cmcglobal.vn>
2022-03-01 08:55:22 +0700

    add new file

---
+++ src/main/java/com/example/jenkin/JenkinApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkin/JenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
