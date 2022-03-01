3839be1f0a14080bd0c536fc6bdd855613c6c1be
xuanbinhcaithe <gaaraxb123@gmail.com>
2022-03-01 09:35:10 +0700

    add say method

--- src/main/java/com/example/jenkin/controller/DemoController.java
+++ src/main/java/com/example/jenkin/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.example.jenkin.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class DemoController {
     @GetMapping("/api")
     public String sayHello() {
         return "Hello world";
+    }
+
+    @GetMapping("/api/{mes}")
+    public String say(@PathVariable("mes") String mes) {
+        return mes;
     }
 }
